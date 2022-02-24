915121a21cdbe4f7fe4d7527e556b958218086a6
oebinu <owam2001@naver.com>
2022-02-24 15:53:04 +0900

    test tf-cloud(6)

---
+++ 01_variable.tf
@@ -0,0 +1,15 @@
+
+
+## 01_variable.tf ##
+
+variable "cluster_name" {
+	default = "cluster_oebinu"
+	type = string
+}
+
+variable "instance_type" {
+ default = "t3.medium"
+ type    = string
+}
+
+
