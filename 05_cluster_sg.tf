5e7c3a7ecccaa94e9c155f4a8181a024441b24a5
oebinu <owam2001@naver.com>
2022-02-24 15:34:27 +0900

    test tf-cloud(3)

--- 05_cluster_sg.tf
+++ 05_cluster_sg.tf
@@ -17,15 +17,3 @@ resource "aws_security_group" "eks" {
 	}
 }
 
-### 기존 코드에 추가 ###
-
-resource "aws_security_group_rule" "eks_cluster_ingress_node_https" {
-  description              = "Allow pods running extension API servers on port 443 to receive communication from cluster control plane."
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks.id
-  source_security_group_id = aws_security_group.worker.id
-  to_port                  = 443
-  type                     = "ingress"
-}
-
